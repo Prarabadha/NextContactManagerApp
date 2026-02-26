42094b0617d089a612469d9633fafeb23bb35d6d
prarabdha bhagat <prarabadhabhagat@gmail.com>
2026-02-26 10:38:49 +0530

    added some changes

---
+++ src/app/contact/edit/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditContactPage = () => {
+  return <div>Edit contact page</div>;
+};
+
+export default EditContactPage;
---
+++ src/app/contact/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewContactPage = () => {
+  return <div>New Contact</div>;
+};
+
+export default NewContactPage;
